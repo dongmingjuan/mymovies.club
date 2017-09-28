569f61b1a996bfa8476a9c53c40535d09efc34f1
dongmingjuan <mingjuandong@mingdeMacBook-Pro.local>
2017-09-28 11:08:16 +0800

    user can create movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,42 @@
 class MoviesController < ApplicationController
+                  before_action :authenticate_user!
+                  def index
+                                   @movies = Movie.all
+                  end
+                  def new
+                                    @movie = Movie.new
+                  end
+                  def create
+                                    @movie = Movie.new(movie_params)
+                                    @movie.user = current_user
+                                    if @movie.save
+                                                      redirect_to movies_path
+                                    else
+                                                      render :new
+                                    end
+                  end
+                  def show
+                                    @movie = Movie.find(params[:id])
+                  end
+                  def edit
+                                    @movie = Movie.find(params[:id])
+                  end
+                  def update
+                                    @movie = Movie.find(params[:id])
+                                    if @movie.update(movie_params)
+                                                      redirect_to movies_path
+                                    else
+                                                      render :edit
+                                    end
+                  end
+                  def destroy
+                                     @movie = Movie.find(params[:id])
+                                     @movie.destroy
+                                     redirect_to movies_path
+                  end
+                  private
+                  def movie_params
+                                    params.require(:movie).permit(:title, :description)
+                  end
+
 end
